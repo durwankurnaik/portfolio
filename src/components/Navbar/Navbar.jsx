ef586ab8ff93ad5bd86b1922e279b1b127d17d8a
Durwankur <ndurwankur14@gmail.com>
2022-04-12 22:53:10 +0530

    Finished implementing Testimonials section. Almost towards the end.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       </div>
 
       <ul className="app__navbar-links">
-        {["home", "about", "work", "skills", "contact"].map((item) => (
+        {["home", "about", "work", "skills", "testimonial", "contact"].map((item) => (
           <li className="app__flex p-text" key={`links-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
