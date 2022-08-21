c88221ffe16a3d148a7222be90a4438cf6315b81
Durwankur Naik <ndurwankur14@gmail.com>
2022-08-21 23:49:29 +0530

    Urgent changes

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: du78kh8i,
   dataset: 'production',
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: "skPsdJamTYQxoRbtfE11BrWouxMdYdoa9AnNdrjJ1y7v1jLDoUJCiwOe7p1IDKJlsODTRn4aiRpe98QLPFEtjfXhJRVE8UMWkOKbI1iOsGuuwvf4men7hln3yBvX5vmofiZUDXQPbKSlCSfkDe0fXasMabOLsmY3UXjLCy6Cae0NKdCoEHyX",
 });
 
 const builder = imageUrlBuilder(client);
